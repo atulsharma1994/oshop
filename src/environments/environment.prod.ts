8a1ffbb31ecad2b46ad66b5abc3231ad37d009ff
atulsharma1994 <atulsharma1208@gmail.com>
2020-09-11 13:48:59 +0530

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD9BqCdoPcVgSPq2IrgCJ3caIKbnTG1hZ0",
+    authDomain: "oshop-af12c.firebaseapp.com",
+    databaseURL: "https://oshop-af12c.firebaseio.com",
+    projectId: "oshop-af12c",
+    storageBucket: "oshop-af12c.appspot.com",
+    messagingSenderId: "174103850007",
+    appId: "1:174103850007:web:df4eca8e39c0ede9aa4052",
+    measurementId: "G-EXRRJBM42Q"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD9BqCdoPcVgSPq2IrgCJ3caIKbnTG1hZ0",
+    authDomain: "oshop-af12c.firebaseapp.com",
+    databaseURL: "https://oshop-af12c.firebaseio.com",
+    projectId: "oshop-af12c",
+    storageBucket: "oshop-af12c.appspot.com",
+    messagingSenderId: "174103850007",
+    appId: "1:174103850007:web:df4eca8e39c0ede9aa4052",
+    measurementId: "G-EXRRJBM42Q"
+  }
 };
