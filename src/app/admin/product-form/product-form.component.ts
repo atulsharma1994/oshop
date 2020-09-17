bd288e63058e30daa3938245650b02b9304685d8
atulsharma1994 <atulsharma1208@gmail.com>
2020-09-17 17:04:10 +0530

    Rendered product list and added product edit feature

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../category.service';
 import { ProductService } from '../../product.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-product-form',
@@ -10,9 +13,22 @@ import { ProductService } from '../../product.service';
 export class ProductFormComponent {
 
   categories$;
+  // Setting it to empty object while saving product no need to get product from backend so,
+  // it would be null and in view we are using two way binding with product properties which results in error.
+  product = {};
 
-  constructor(private categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private categoryService: CategoryService,
+    private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
+
+    let id = this.route.snapshot.paramMap.get('id');
+    // Uses take here so that it will subscribe event to only once hence no need to unsubscribe again.
+    if (id) this.productService.get(id).take(1).subscribe(product => {
+      this.product = product
+    });
   }
 
   save(product) {
