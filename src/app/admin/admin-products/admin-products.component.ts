c579df820a345471e318c6237657af1e45c51e95
atulsharma1994 <atulsharma1208@gmail.com>
2020-09-19 22:43:50 +0530

    Added search bar for manage products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,20 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../product.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  products$;
+  products: Product[];
+  filterProducts: any[];
+  subscription: Subscription;
 
   constructor(productService: ProductService) {
-    this.products$ = productService.getAll()
+    this.subscription = productService.getAll()
+      .subscribe(products => {
+        this.filterProducts = this.products = products;
+      });
   }
 
   ngOnInit() {
+  }
+
+  // Filtering based on title
+  filter(query: string) {
+    this.filterProducts = (query) ?
+      this.products.filter(product => product.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
