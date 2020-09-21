2bc6c0b65b6b5310681dfd35d72ad003c3ac02e7
atulsharma1994 <atulsharma1208@gmail.com>
2020-09-21 17:03:41 +0530

    Handled multiple async calls and added All Categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
@@ -10,11 +13,26 @@ import { CategoryService } from '../category.service';
 export class ProductsComponent {
 
   products$;
+  filteredProducts: Product[] = [];
+  products: Product[] = [];
   categories$;
+  category: string;
 
-  constructor(productService: ProductService, categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+  constructor(productService: ProductService, categoryService: CategoryService, router: ActivatedRoute) {
+    productService.getAll()
+      .switchMap((products: any) => {
+        this.products = products;
+        return router.queryParamMap;
+      }).subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = (this.category) ?
+          this.products.filter(product => product.category === this.category) :
+          this.products
+      });
+
     this.categories$ = categoryService.getAll();
+
   }
 
 }
